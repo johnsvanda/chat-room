c9d9bcc0ef1cab31d0b09de4cb00b47a08df3825
Jan Švanda <john.svanda@gmail.com>
2020-01-09 11:04:51 +0100

    build

---
+++ build/precache-manifest.1ac89fb79dcd7fb1f69f7658ccb1098c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0595968d894f09aa12b1f97ccbda62f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85e0a5833c2522cfe260",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "30ee815b44e033e79607",
+    "url": "/static/css/main.bb628663.chunk.css"
+  },
+  {
+    "revision": "85e0a5833c2522cfe260",
+    "url": "/static/js/2.cf952c12.chunk.js"
+  },
+  {
+    "revision": "19a8c5c90e14303bec2a9b6db3fea5ac",
+    "url": "/static/js/2.cf952c12.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30ee815b44e033e79607",
+    "url": "/static/js/main.14e441ff.chunk.js"
+  },
+  {
+    "revision": "65326020070271871721",
+    "url": "/static/js/runtime-main.2df13cd9.js"
+  },
+  {
+    "revision": "30c83191dab29787ce5ec57ca4524915",
+    "url": "/static/media/background.30c83191.jpg"
+  }
+]);
