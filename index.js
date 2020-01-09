3b79bcee678b22e0a0c1d941fdb478464b39f85b
Jan Švanda <john.svanda@gmail.com>
2020-01-09 11:03:16 +0100

    final

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("Server is running on port: " + PORT));
+const server = app.listen(PORT, () =>
+  console.log("Server is running on port: " + PORT)
+);
+
+const io = require("socket.io")(server, { origins: "*:*" });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "build")));
+
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  });
+}
+
+io.on("connection", socket => {
+  console.log("a user connected");
+  socket.on("join", ({ name, chatroom }) => {
+    socket.join(chatroom);
+    console.log(`${name} has joined ${chatroom}`);
+
+    socket.on("chat-message", message => {
+      socket.broadcast.to(chatroom).emit("chat-message", message);
+    });
+  });
+
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
+  });
+});
